ddd1eadb70aa7781abc3f91e621ee91a90a1d890
Sandanila <gsandanila@gmail.com>
2025-12-09 17:41:40 +0530

    initialization 🚀

---
+++ controllers/userController.js
@@ -0,0 +1,103 @@
+import User from "../models/user.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+export function createUser(req,res){
+
+    const hashedPassword = bcrypt.hashSync(req.body.password , 10);
+
+    const user = new User(
+        {
+            email : req.body.email,
+            firstName : req.body.firstName,
+            lastName : req.body.lastName,
+            password : hashedPassword
+        }
+    )
+
+    user.save().then(
+        ()=>{
+            res.json({ message : "User created successfully" });
+        }
+    ).catch(
+        (error)=>{
+            res.json({ message : "Error creating user", error : error });
+        }
+    );
+}
+
+export function loginUser(req,res){
+
+
+
+    User.findOne(
+        {
+            email : req.body.email
+        }
+    ).then(
+        (user)=>{
+            if(user == null){
+                res.json({
+                    message : "User with given email not found"
+                })
+            }else{
+                const isPasswordValid = bcrypt.compareSync(
+                    req.body.password , 
+                    user.password
+                );
+                
+                if(isPasswordValid){
+
+                    const token = jwt.sign(
+                        {
+                            email : user.email,
+                            firstName : user.firstName,
+                            lastName : user.lastName,
+                            role : user.role,
+                            image : user.image,
+                            isEmailVerified : user.isEmailVerified
+                        } , "i-computers-54!")
+
+                    console.log(token)
+
+                    console.log(
+                        {
+                            email : user.email,
+                            firstName : user.firstName,
+                            lastName : user.lastName,
+                            role : user.role,
+                            image : user.image,
+                            isEmailVerified : user.isEmailVerified
+                        } );
+
+
+                    res.json({
+                        message : "Login successful",
+                        token : token
+                    });
+                }else {
+                    res.status(401).json(
+                        {
+                            message : "Invalid password"
+                        }
+                    );
+            }
+        }
+    })
+    .catch(()=> {
+        res.status(500).json({
+            message : "Internal server error",
+        });
+    });
+}
+
+export function isAdmin(req, res) {
+    
+    if(req.user == null){
+        return false
+    }
+    if(req.user.role == "admin"){
+        res.ststus(403).json({message : "You are not authorized to create a product"})
+        return
+    }
+}
---
+++ index.js
@@ -0,0 +1,39 @@
+import express from 'express'
+import mongoose from "mongoose"
+import userRouter from './router/userRouter.js'
+import productRouter from './router/productRouter.js'
+import authorizeUser from './lib/jwtMiddleware.js'
+
+
+
+const mongoURI = "mongodb+srv://admin:1234@cluster0.x1lexni.mongodb.net/?appName=Cluster0"
+
+mongoose.connect(mongoURI).then(
+    ()=>{
+        console.log("Connected to MongoDB")
+    }
+).catch(
+    ()=>{
+        console.log("Error connecting to MongoDB")
+    }
+)
+
+const app = express()
+
+app.use(express.json())
+
+app.use(authorizeUser)
+
+app.use("/users", userRouter)
+app.use("/products", productRouter)
+
+
+
+function start(){
+    console.log("Sever is running on port 3000")
+}
+
+
+
+
+app.listen(3000, start)
